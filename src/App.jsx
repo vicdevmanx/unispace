3443edbe639e35fbf6d00793be1dffe8b6551362
thatdevelopergirlcobham <cobhamdawn00@gmail.com>
2025-06-21 23:06:37 +0100

    proper auth routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,3 @@
-<<<<<<< Updated upstream
-import About from './components/About';
-import { ContactUs } from './components/ContactUs';
-import FAQ from './components/Faq';
-import Footer from './components/Footer';
-import HeroSection from './components/HeroSection';
-import Navbar from './components/Navbar';
-import Services from './components/services';
-import './index.css';
-
-function App() {
-  return (
-    <div className="bg-[#1D3A8A]/5">
-     <Navbar />
-
-     <HeroSection/>
-
-     <section id='about'>
-        <About />
-      </section>
-
-      <section id='service'>
-        <Services />
-      </section>
-
-      <section id='faq'>
-        <FAQ />
-      </section>
-
-      <section id='contac'>
-        <ContactUs />
-      </section>
-
-      <Footer/>
-    </div>
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
@@ -51,7 +15,6 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
->>>>>>> Stashed changes
   );
 }
 
