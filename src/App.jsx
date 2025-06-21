20dac1e1269536b9f20f6c21e5276e7ee5ffb63c
princeobeten <princeobeten56@gmail.com>
2025-06-21 19:47:13 +0100

    Update SVG logos and enhance layout in App component
    - Updated the dimensions and structure of the unispace_logo.svg and unispace_white_logo.svg files for improved scalability.
    - Replaced text elements with path elements in SVGs for better rendering.
    - Modified the App component to include a new HeroSection component, enhancing the layout.
    - Improved the FAQ section layout with responsive design adjustments and added an image alongside the FAQ cards.
    - Introduced a custom font 'Lequire' in the CSS for better typography.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import FAQ from './components/Faq';
 import Hero from './components/Hero';
+import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
 import './index.css';
 
 function App() {
   return (
-    <div className="bg-[#1D3A8A]/10 min-h-screen relative">
-      <div className="pattern bg-[#1D3A8A] pt-16 pb-40">
-        <Navbar />
-        <Hero />
-      </div>
-
-      
+    <div className="bg-[#1D3A8A]/10">
+     <Navbar />
+     <HeroSection/>
       <section id='faq'>
         <FAQ />
       </section>
