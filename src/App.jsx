60f545d2aa72b662176421afbf7e750b2ddc674e
Kante <esemeudo372@gmail.com>
2025-06-21 23:18:09 +0100

    trying to pull so i was told to push first

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import HeroSection from './components/HeroSection'
 import  Navbar from './components/Navbar'
 import './index.css'
 import Footer from './components/Footer'
+import About from './components/About'
+import { ContactUs } from './components/ContactUs'
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
     <div className="bg-[#1D3A8A]/10">
       <Navbar/>
       <HeroSection/>
+      <About/>
+      <ContactUs/>
     </div>
   )
 }
