c10f348d33feccb245a573405d4d6e1811e5f4e5
Kante <esemeudo372@gmail.com>
2025-06-23 07:37:46 +0100

    fixed CTA

--- src/Pages/workspace-service/pages/workspace-service.tsx
+++ src/Pages/workspace-service/pages/workspace-service.tsx
@@ -1,10 +1,12 @@
 import WorkspaceLayout from "../workspace-layout";
+import WorkspaceCta from "../components/workspace-cta";
 
 const WorkspaceService = () => {
   return (
-  <WorkspaceLayout>
-    WorkspaceService
-  </WorkspaceLayout>)
+    <WorkspaceLayout>
+      <WorkspaceCta />
+    </WorkspaceLayout>
+  );
 };
 
 export default WorkspaceService;
