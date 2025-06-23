85c65bed05f3569f293dbb446fc7b869527673cd
Kante <esemeudo372@gmail.com>
2025-06-23 21:22:29 +0100

    pushing user booking UI

--- src/Pages/workspace-service/pages/workspace-service.tsx
+++ src/Pages/workspace-service/pages/workspace-service.tsx
@@ -1,10 +1,12 @@
 import WorkspaceLayout from "../workspace-layout";
 import WorkspaceCta from "../components/workspace-cta";
+import CreateBooking from "../components/CreateBooking";
 
 const WorkspaceService = () => {
   return (
     <WorkspaceLayout>
-      <WorkspaceCta />
+      {/* <WorkspaceCta /> */}
+      <CreateBooking/>
     </WorkspaceLayout>
   );
 };
